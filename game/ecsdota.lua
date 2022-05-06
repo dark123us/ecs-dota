5c82667e6097804e93de0254d2603ca6e46b3085
Vadzim Pryhodzich <7676562@gmail.com>
2022-05-06 08:46:18 +0300

    fix

--- game/ecsdota.lua
+++ game/ecsdota.lua
@@ -2,7 +2,6 @@ local args = {...}
 local directory = args[1]
 print('start ecsdota.lua', directory)
 
-
 local module = {}
 for _, n in pairs({'component', 'entity', 'event', 'system', 'world'}) do
     module[n] = require (directory.. '.'..n )
